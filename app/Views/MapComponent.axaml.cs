9ac8e5faf2f7e533d8ca4498cac25c46cf440c84
SV 10/2020 <nemanja.majstorovic3214@gmail.com>
2023-05-28 11:34:09 +0200

    Added map

---
+++ app/Views/MapComponent.axaml.cs
@@ -0,0 +1,21 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace app.Views;
+
+public partial class MapComponent : UserControl
+{
+    public MapComponent()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+        var mapControl = new Mapsui.UI.Avalonia.MapControl();
+        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        Content = mapControl;
+    }
+}
