57738b0f917279149ec259c1e3df6725c7f671c2
eXtremeNemanja <dutina.sv27.2020@uns.ac.rs>
2023-06-13 07:41:32 +0200

    feat: trip creation validation

--- app/ViewModels/Trip/TripCreateFormViewModel.cs
+++ app/ViewModels/Trip/TripCreateFormViewModel.cs
@@ -1,4 +1,6 @@
+using ReactiveValidation;
 using System;
+using ReactiveValidation.Extensions;
 
 namespace app.ViewModels;
 
@@ -9,24 +11,29 @@ public class TripCreateFormViewModel : BaseViewModel
     public string Title
     {
         get => _title;
-        set { _title = value ?? throw new ArgumentNullException(nameof(value)); OnPropertyChanged(nameof(Title)); }
+        set {
+            _title = value;
+            OnPropertyChanged(nameof(Title));
+            IsButtonEnabled();
+        }
     }
 
     public string Description
     {
         get => _description;
         set  { _description = value ?? throw new ArgumentNullException(nameof(value));
-            OnPropertyChanged(nameof(value));
+            OnPropertyChanged(nameof(Description));
         }
 }
 
     private string _description;
     public TripCreateFormViewModel()
     {
-        Title = "Naziv";
-        Description = "Opis";
+        //Title = "Naziv";
+        //Description = "Opis";
         SelectedDate = DateTime.Now;
-        Price = 0;
+        Validator = GetValidator();
+        //Price = 0;
     }
 
     private Double _price;
@@ -38,23 +45,76 @@ public class TripCreateFormViewModel : BaseViewModel
     {
         get => _price;
         set { _price = value; 
-        OnPropertyChanged(nameof(value));
-    }
+        OnPropertyChanged(nameof(Price));
+        IsButtonEnabled();
+        }
 }
 
     public DateTime SelectedDate
     {
         get => _selectedDate;
         set { _selectedDate = value; 
-        OnPropertyChanged(nameof(value));
-    }
+        OnPropertyChanged(nameof(SelectedDate));
+        IsButtonEnabled();
+        }
 }
 
     public int Lasting
     {
         get => _lasting;
-        set { _lasting = value; 
-        OnPropertyChanged(nameof(value));
+        set { 
+            _lasting = value; 
+            OnPropertyChanged(nameof(Lasting));
+            IsButtonEnabled();
+        }
     }
-}
+
+    private IObjectValidator GetValidator()
+    {
+        var builder = new ValidationBuilder<TripCreateFormViewModel>();
+
+        builder.RuleFor(vm => vm.Title)
+            .NotEmpty()
+            .WithMessage("Obavezno polje")
+            .AllWhen(vm => vm.Title != null);
+
+        builder.RuleFor(vm => vm.Price)
+            .GreaterThan(0)
+            .WithMessage("Cena mora biti pozitivna")
+            .AllWhen(vm => vm.Price != null);
+
+        builder.RuleFor(vm => vm.Lasting)
+            .GreaterThan(0)
+            .WithMessage("Trajanje mora biti pozitivno")
+            .AllWhen(vm => vm.Lasting != null);
+
+        builder.RuleFor(vm => vm.SelectedDate)
+            .Must(checkDate)
+            .WithMessage("Datum mora biti u buducnosti");
+
+        //builder.RuleFor(vm => vm.Query)
+        //    .Must(validLocation)
+        //    .WithMessage("Obavezno polje")
+        //    .AllWhen(vm => vm.SelectedLocation == null && vm.Query != null);
+
+        return builder.Build(this);
+    }
+
+    private static bool checkDate(DateTime dateTime)
+    {
+        if (dateTime == null)
+        {
+            return false;
+        }
+        bool b = DateOnly.FromDateTime(dateTime).CompareTo(DateOnly.FromDateTime(DateTime.Now)) > 0;
+        return b;
+    }
+
+    public bool ButtonEnabled { get; set; }
+
+    public void IsButtonEnabled()
+    {
+        ButtonEnabled = Title != null && Title.Length > 0 && Price != null && Price > 0 && Lasting != null && Lasting > 0 && checkDate(SelectedDate);
+        OnPropertyChanged(nameof(ButtonEnabled));
+    }
 }
